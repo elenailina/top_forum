e4c730107df54f1732f9faa24b56af09ff662c40
Elena <elenailina91@yahoo.com>
2021-10-22 11:19:37 +0300

    01_index

--- src/js/script.js
+++ src/js/script.js
@@ -7,36 +7,27 @@ $(document).ready(function(){
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="next"><img src="icons/right.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
-        ]
     });
-  });
-      
-
-
-
-
-const slider = tns({
-    container: '.promo__slide',
-    items: 1,
-    slideBy: 'page',
-    autoplay: false,
-    controls: false,
+    $('.clients__wrapper').slick({
+        infinite: true,
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        speed: 1200,
+        prevArrow: '<button type="button" class="prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="next"><img src="icons/right.svg"></button>',
+    });
+    $('.promo__wrapper').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 1200,
+        adaptiveHeight: false,
+        prevArrow: '<button type="button" class="prev"><img src="icons/left_white.svg"></button>',
+        nextArrow: '<button type="button" class="next"><img src="icons/right_white.svg"></button>',
+    });
 });
 
-document.querySelector('.prev').addEventListener('click', function () {
-    slider.goTo('prev');
-});
-document.querySelector('.next').addEventListener('click', function () {
-    slider.goTo('next');
-});
+
 
 const hamburger = document.querySelector('.hamburger'),
            menu = document.querySelector('.menu'),
