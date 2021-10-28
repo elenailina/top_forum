a7117eb3377b33a0a71909e855e0c51d7e76f697
Elena <elenailina91@yahoo.com>
2021-10-28 08:24:37 +0300

    register&modal

--- src/js/script.js
+++ src/js/script.js
@@ -27,36 +27,9 @@ $(document).ready(function(){
     });
 });
 
-
-$(document).ready(function(){
-    $("#menu").on("click","a", function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top-151}, 1500);
-    });
+$('[data-modal=subscribe]').on('click', function() {
+    $('.overlay, #subscribe').fadeIn('slow');
 });
-
-const sponsors = document.querySelector('.sponsors');
-sponsors.addEventListener('click', () => {
-    menu.classList.add('display: block');
-});
-
-const hamburger = document.querySelector('.hamburger'),
-           menu = document.querySelector('.menu'),
-           closeElem = document.querySelector('.menu__close');
-
-hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
-});
-
-closeElem.addEventListener('click', () => {
-    menu.classList.remove('active');
-});
-
-const counters = document.querySelectorAll('.skills__rating-counter'),
-    lines = document.querySelectorAll('.skills__rating-line span');
-
-counters.forEach( (item, i) => {
-    lines[i].style.width = item.innerHTML;
+$('.modal__close').on('click', function(){
+    $('.overlay, #subscribe').fadeOut('slow');
 });
